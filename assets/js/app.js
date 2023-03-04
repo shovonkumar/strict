5bd172f9bb6106cef27a52b227ca429362fc1d0c
Shovon Kumar <124199424+shovonkumar@users.noreply.github.com>
2023-03-04 23:42:43 +0600

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+    $(window).on('scroll', function(){
+        var scroll = $(this).scrollTop();
+        if(scroll > 400) {
+            $('.back_to_top').fadeIn()
+            $('.navbar').addClass('nav_bg_sticky')
+        }
+        else {
+            $('.back_to_top').fadeOut()
+            $('.navbar').removeClass('nav_bg_sticky')
+        }
+    });
+
+    $('.back_to_top').click(function(){
+        $('html, body').animate({scrollTop:0});
+    });
+
+    // mixitup activation code
+	var mixer = mixitup('.myfilter');
+});
+
+
+
+// WOW JS
+
+new WOW().init();
+
+
+// Venobox code start
+
+new VenoBox({
+    selector: '.my-image-links',
+    numeration: true,
+    infinigall: true,
+    share: true,
+    spinner: 'rotating-plane'
+});
+
+// venobox code end
